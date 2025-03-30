d2e4ed4151e519cbd15fbf933b9ed0806681a93d
Radina Kazakova <147272490+radinakazakova@users.noreply.github.com>
2025-03-30 16:56:48 +0300

    Update ships.sql

--- Week_03/Tasks/ships.sql
+++ Week_03/Tasks/ships.sql
@@ -4,7 +4,7 @@
 SELECT DISTINCT COUNTRY
 FROM CLASSES
 WHERE NUMGUNS >= ALL (SELECT NUMGUNS
-					  FROM CLASSES)
+		      FROM CLASSES)
 
 --Напишете заявка, която извежда класовете, за които поне един от
 --корабите е потънал в битка.
@@ -13,14 +13,14 @@ SELECT DISTINCT CLASSES.CLASS
 FROM CLASSES JOIN SHIPS --свързваме всеки кораб с класът му
 	ON CLASSES.CLASS = SHIPS.CLASS 
 WHERE NAME IN (SELECT SHIP
-				FROM OUTCOMES
-				WHERE RESULT = 'sunk') --всички, които са потънали
+	       FROM OUTCOMES
+	       WHERE RESULT = 'sunk') --всички, които са потънали
 
 --Напишете заявка, която извежда името и класа на корабите с 16 инчови оръдия.
 
 SELECT NAME, CLASSES.CLASS
 FROM CLASSES JOIN (SELECT CLASS, NAME
-					FROM SHIPS) AS SHIP
+		   FROM SHIPS) AS SHIP
 	ON CLASSES.CLASS = SHIP.CLASS
 WHERE BORE = 16
 
@@ -28,22 +28,29 @@ WHERE BORE = 16
 --участвали кораби от клас ‘Kongo’.
 
 SELECT BATTLE
-FROM BATTLES JOIN ( SELECT SHIP, BATTLE, CLASS
-					FROM OUTCOMES JOIN (SELECT CLASS, NAME
-										FROM SHIPS) AS SHIP2
-						ON SHIP2.NAME = OUTCOMES.SHIP) AS SHIP_IN_BATTLE
+FROM BATTLES JOIN (SELECT SHIP, BATTLE, CLASS
+		   FROM OUTCOMES JOIN (SELECT CLASS, NAME
+				       FROM SHIPS) AS SHIP2
+			ON SHIP2.NAME = OUTCOMES.SHIP) AS SHIP_IN_BATTLE
 	 ON BATTLES.NAME = SHIP_IN_BATTLE.BATTLE
 WHERE CLASS = 'Kongo'
+
+SELECT DISTINCT BATTLE
+FROM BATTLES JOIN OUTCOMES
+	ON BATTLES.NAME = OUTCOMES.BATTLE
+WHERE SHIP IN (SELECT NAME
+	       FROM SHIPS
+	       WHERE CLASS='Kongo')
 
 --Напишете заявка, която извежда класа и името на корабите, чиито брой
 --оръдия е по-голям или равен на този на корабите със същия калибър оръдия.
 
 SELECT CLASS, NAME
 FROM SHIPS
-WHERE CLASS IN ( SELECT CLASS
-				 FROM CLASSES AS CL
-				 WHERE CL.NUMGUNS >= ALL(SELECT NUMGUNS
-										 FROM CLASSES
-										 WHERE CLASSES.BORE = CL.BORE)
-				)
+WHERE CLASS IN (SELECT CLASS
+		FROM CLASSES AS CL
+		WHERE CL.NUMGUNS >= ALL(SELECT NUMGUNS
+					FROM CLASSES
+					WHERE CLASSES.BORE = CL.BORE)
+		)
 ORDER BY CLASS --само заради подредбата в отговора на задачата
